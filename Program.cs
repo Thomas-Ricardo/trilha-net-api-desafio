ffd1cae54d5fe669d4b00155782be3941eb93d1c
Thomas-Ricardo <thomasricardo1000@hotmail.com>
2026-09-22 07:41:58 -0300

    challenge solved

--- Program.cs
+++ Program.cs
@@ -1,32 +1,27 @@
-using System.Text.Json.Serialization;
-using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddDbContext<OrganizadorContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
-builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// ALTERAÇÃO 1: Trocar AddOpenApi pelo SwaggerGen tradicional
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // ALTERAÇÃO 2: Trocar MapOpenApi pelo UseSwagger padrão
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
